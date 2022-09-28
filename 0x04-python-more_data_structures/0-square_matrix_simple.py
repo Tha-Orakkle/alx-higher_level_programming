2fa5a65ff9bb45f69976669c4de0e26d3ffed854
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-09-28 21:40:14 +0100

    squares matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [num**2 for row in matrix for num in row]
+    new_matrix = [[num**2 for num in row] for row in matrix]
     return new_matrix
