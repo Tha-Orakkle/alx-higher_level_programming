cfe9469af71e11c8b290745b96d050defc1bd464
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-09-29 00:31:52 +0000

    returns a key with the biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
+    if not a_dictionary:
+        return None
+    else
         max = list(a_dictionary)[0]
         for n in list(a_dictionary):
             if n > max:
                 max = n
         return max
-    else:
-        return None
