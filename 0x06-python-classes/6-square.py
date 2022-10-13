ac7ebf4d2ce4de690ef5de9d56408ac08695d8fa
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-10-13 05:37:42 +0100

    Coordinates of a square fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,7 +20,7 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """initializes the data"""
         self.__size = size
-        self.__position = position
+        self.position = position
 
     @property
     def size(self):
