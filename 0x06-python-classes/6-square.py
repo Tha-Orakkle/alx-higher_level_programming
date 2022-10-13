bfbf041a8b2a4d45090fecd129ed672907df4f9a
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-10-13 11:18:02 +0100

    Errors Fixed

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,8 +19,8 @@ class Square:
     """
     def __init__(self, size=0, position=(0, 0)):
         """initializes the data"""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
