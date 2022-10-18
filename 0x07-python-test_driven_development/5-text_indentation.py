707c26fd26df3ba745a904d478b6235f612697d7
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-10-18 14:48:49 +0000

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+Module text_indentation
+Adds two new lines after each of the characters(.:?)
+"""
+
+
+def text_indentation(text):
+    """Prints text with two newlines
+    after each of these characters {'.', '?', ':'}.
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
