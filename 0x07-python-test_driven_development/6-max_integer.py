d046384150d89bb49033f7b8f6c4f226e45b12c0
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-10-19 04:34:38 +0000

    matrix multiplication

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """Module to find the max integer in a list
 """
 
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """Unittest for the max_integer module
 """
 
