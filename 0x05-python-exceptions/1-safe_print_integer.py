61b7181489b1c2a0c6762eff65b221354d54e80d
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-01-24 00:20:12 -0300

    testing task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
-        return False
+    except ValueError:
+        return false
     else:
         return True
