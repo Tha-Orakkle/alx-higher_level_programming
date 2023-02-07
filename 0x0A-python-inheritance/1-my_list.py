b9dcff281d49bb9e4532007d302f7a2c7b34f2d3
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-02-07 17:00:34 +0100

    task 2: finishing touches

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,7 @@
 """Defines an inherited list class Mylist."""
 
 
-class Mylist(list):
+class MyList(list):
     """Implements sorted printing for the built_in list class."""
 
     def print_sorted(self):
