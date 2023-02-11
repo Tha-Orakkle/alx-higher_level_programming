81ab836b9f25127c17da13c4a9aa66cfae4b5905
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-02-11 19:36:19 +0000

    some changes effected

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Defines a first class base"""
+
+
+class Base:
+    """The Base model
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initializes a new Base
+
+        Args:
+            id (int): identity of the base
+        """
+        if id is not  None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
