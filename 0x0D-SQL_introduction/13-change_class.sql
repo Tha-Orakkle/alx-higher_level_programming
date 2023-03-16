6b0ce72ce017887411db1b578bd7f710f4a3d409
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-03-16 09:15:59 +0000

    SQL: tasks 12 through 16

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Removes all records with a score <= 5 in the table second_tableof the database in your MySQL server.
+DELETE FROM `second_table`
+WHERE `score` <= 5;
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- Lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- The number of records labeled as number.
+-- Records are ordered in descending order.
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+GROUP BY `score` DESC;
