ad5fa16c38c5b95518da5654dc14ae46c0cd6010
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-05-20 04:46:34 -0700

    some typpo fixes made to task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,11 +6,11 @@ Lists all states in the database hbtn_0e_0_usa
 import sys
 import MySQLdb
 
-id __name__ == "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cursor.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states")
     states = cur.fetchall()
     for state in states:
         print(state)
