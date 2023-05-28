438c0cfb551626e798235fce44963965754d03ba
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-05-28 01:44:39 +0100

    lists all cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+LIsts all cities from the database hbtn_0e_4_usa
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cur = db.cursor()
+
+    cur.execute("SELECT c.id, c.name, s.name \
+                FROM cities c JOIN state s ON c.state_id = s.id;")
+    cities = cur.fetchall()
+
+    for city in cities:
+        print(city)
+
+    cur.close()
+    db.close()
