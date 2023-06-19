d3a8b826203d499ca26b6abaaba216fe762b3787
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-06-19 00:50:49 -0700

    tasks 12: changing the name of a state object

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-adds the state object `Louisiana` to the database hbtn_0e_6_usa
+changes the name of a stste objecr from the database hbtn_0e_6_usa
 """
 
 import sys
@@ -8,17 +8,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name="Louisiana")
-    session.add(new_state)
+    x = session.query(State).get(2)
+    x.name = 'New Mexico'
     session.commit()
-
-    print(new_state.id)
