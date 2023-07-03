ff29cc83de645519848d8c52ff8bd8924d7ee941
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-07-03 02:21:41 -0700

    caught You because I could

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL as an argument, sends a POST request to the URL, and displays the body of the response
-curl -sL -X PUT -d "You got me!" http://0.0.0.0:5000/catch_me | grep "You got me!" | cut -d "'" -f 2 | cut -c -11
+curl -sL -X PUT -d "You got me!" http://0.0.0.0:5000/catch_me | grep "You got me!" | cut -d "'" -f 2 | cut -c -11 | tr -d '\n'
