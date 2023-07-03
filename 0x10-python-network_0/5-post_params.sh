8ce94382505c7a7619d29183b3c32b723b46c209
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-07-02 17:27:19 -0700

    HTTP requests

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # sends a POST request wth variables, and displays the body of the response
 curl -s -X POST -d "email: test@gmail.com" -d "subject: I will always be here for PLD" "$1"
